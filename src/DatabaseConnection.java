a2a083f16ee97ed812fcff1de0bd7825c3251e88
simararustam <simararustam@gmail.com>
2024-09-09 17:54:29 +0400

    updating

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -4,8 +4,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:8080/todo_list";
-    private static final String USER = "root"; // Replace with your MySQL username
-    private static final String PASSWORD = "simoshh777"; // Replace with your MySQL password
+    private static final String USER = "XXX"; // Replace with your MySQL username
+    private static final String PASSWORD = "XXX"; // Replace with your MySQL password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
